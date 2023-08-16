298f88c5ed87b245a151fbd774205341c11261b4
Asal <asal.salmani@yahoo.com>
2023-08-16 13:40:22 +0330

    Changing list.of method in ProfessorsConfigure and StudentConfigure.

--- src/main/java/com/example/CourseManagment/Configures/ProfessorsConfigure.java
+++ src/main/java/com/example/CourseManagment/Configures/ProfessorsConfigure.java
@@ -8,24 +8,26 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
 public class ProfessorsConfigure {
     @Bean
-    public CommandLineRunner commandLinerunnerP(ProfessorsRepository professorsRepository){
+    public CommandLineRunner commandLinerunnerP(ProfessorsRepository professorsRepository) {
+
         return args -> {
-            Professers a=new Professers(
+            Professers a = new Professers(
                     144444,
                     "Maryam",
                     "Khodabakhsh",
                     2121
-
-
             );
 
-            professorsRepository.saveAll(List.of(a));
+            List<Professers> professorsList = new ArrayList<>(); // Create a list
+            professorsList.add(a); // Add the professor to the list
 
+            professorsRepository.saveAll(professorsList); // Save the list of professors
         };
     }
 }
--- src/main/java/com/example/CourseManagment/Configures/StudentConfigure.java
+++ src/main/java/com/example/CourseManagment/Configures/StudentConfigure.java
@@ -6,6 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.ArrayList;
 import java.util.List;
 @Configuration
 public class StudentConfigure {
@@ -22,20 +23,22 @@ public class StudentConfigure {
    public CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
         return args -> {
             Student asal = new Student(
-                    "asa;",
-                    "salamni",
-                    0200010,
+                    "Asal",
+                    "Salamni",
+                    200010,
                     "Iran.Tehran");
             Student asal2 = new Student(
-                    "asa;",
-                    "salamni",
-                    0200010,
+                    "Asal",
+                    "Salamni",
+                    200011,
                     "Iran.Tehran");
 
-            studentRepository.saveAll(List.of(asal, asal2));
+            List<Student> studentsList = new ArrayList<>(); // Create a list
+            studentsList.add(asal); // Add the first student to the list
+            studentsList.add(asal2); // Add the second student to the list
 
+            studentRepository.saveAll(studentsList); // Save the list of students
         };
-
 
     }
 }
